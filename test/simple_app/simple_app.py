75b7eeff1690cf62ce622658276ef200a439295c
misha.gavela <arfey17.gm@gmial.com>
2017-08-26 21:33:48 +0300

    feat: corrected .travis.yml

--- test/simple_app/simple_app.py
+++ test/simple_app/simple_app.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from threading import Thread
+import time
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-if __name__ == '__main__':
+def run():
     app.run(host='0.0.0.0')
+
+if __name__ == '__main__':
+    t = Thread(target=run)
+    t.start()
+    time.sleep(2)
+    t.join()
